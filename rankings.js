fa7ef2aaa19f1e0cb88168c4f68ffee0a21b18b4
Jackson Norris <jacksonnorris@Jacksons-MacBook-Pro.local>
2019-08-24 18:03:32 -0400

    h

--- rankings.js
+++ rankings.js
@@ -9,21 +9,31 @@ function(err){
 
 
 var useData = function(data){
-  console.log(data)
+  width = 400;
+  height = 3000;
+
+console.log(data)
   var svg = d3.select('svg')
-  .attr('width',200)
-  .attr('height',200)
+  .attr('width',width)
+  .attr('height',height)
 
 var plot = svg.append('g')
-.attr('width',200)
-.attr('height',200)
+.attr('width',width)
+.attr('height',height)
 
-plot.append('text')
+plot.selectAll('text')
 .data(data)
+.enter()
+.append('text')
+.text(function(d,i){return d.rank + '. ' + d.AverName + ' ' + d.Aver})
+.attr('font-size',12)
 .attr('x',20)
-.attr('y',20)
-.attr('fill','white')
-//.text(function(data){return data.AverName })
+.attr('y',function(d,i){return 20 + 13*i})
+.attr('fill','black')
 
+var table = svg.append('tableName').selectAll('table')
+.data(data)
+.enter()
+.append('table')
 
 }
