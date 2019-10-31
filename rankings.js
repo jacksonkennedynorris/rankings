3e428ab9d98c493a87cd81832062ff135617f253
Jackson Norris <jacksonnorris@Jacksons-MacBook-Pro.local>
2019-10-30 21:13:17 -0400

    h

--- rankings.js
+++ rankings.js
@@ -1,6 +1,5 @@
 var tableData = d3.csv('genFile.csv')
 
-
 tableData.then(function(data){
   useData(data)
 },
@@ -8,16 +7,43 @@ function(err){
   console.log(err)
 });
 
-
 var useData = function(data){
   width = 500;
-  height = 4500;
+  height = 300;
 
-console.log(data)
   var svg = d3.select('svg')
   .attr('width',width)
   .attr('height',height)
 
+var myTab = d3.select('table.totalTable')
+
+var tableNames = ["Ranking","Class","School","Rating","In-State Record","Out-of-State Record"];
+
+var header = myTab.append('thead')
+.selectAll('th')
+.data(tableNames)
+.enter()
+.append('th')
+.text(function(d){return d;})
+
+var tableBody = myTab.append('tbody')
+var row = tableBody.selectAll('tr')
+.data(data)
+.enter()
+.append('tr')
+.text(function(d,i){return d.Rank})
+row.append('td')
+.text(function(d,i){return d.Class})
+row.append('td')
+.text(function(d,i){return d.Name})
+row.append('td')
+.text(function(d,i){return d.Rating})
+row.append('td')
+.text(function(d,i){return d.InState})
+row.append('td')
+.text(function(d,i){return d.Total})
+
+/*
 var plot = svg.append('g')
 .attr('width',width)
 .attr('height',height)
@@ -31,10 +57,5 @@ plot.selectAll('text')
 .attr('x',20)
 .attr('y',function(d,i){return 50 + 20*i})
 .attr('fill','black')
-
-var table = svg.append('tableName').selectAll('table')
-.data(data)
-.enter()
-.append('table')
-
+*/
 }
