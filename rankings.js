338d3758b70ccfae507a15ffdb49be946724e1b1
Jackson Norris <jacksonnorris@Jacksons-MacBook-Pro.local>
2019-11-04 14:07:23 -0500

    j

--- rankings.js
+++ rankings.js
@@ -1,43 +1,41 @@
 var tableData = d3.csv('genFile.csv')
-
-tableData.then(function(data){useData(data)},
-
-function(err){console.log(err)});
+  tableData.then(function(data){useData(data)},
+  function(err){console.log(err)});
 
 var useData = function(data){
-  width = 500;
-  height = 300;
+    width = 500;
+    height = 1000;
 
-  var svg = d3.select('svg')
-  .attr('width',width)
-  .attr('height',height)
+    var svg = d3.select('svg')
+    .attr('width',width)
+    .attr('height',height)
 
-var myTab = d3.select('table.totalTable')
+  var myTab = d3.select('table.totalTable')
 
-var tableNames = ["Ranking","Class","School","Rating","In-State Record","Out-of-State Record"];
+  var tableNames = ["Ranking","Class","School","Rating","In-State Record","Out-of-State Record"];
 
-var header = myTab.append('thead')
-  .selectAll('th')
-  .data(tableNames)
-  .enter()
-  .append('th')
-  .text(function(d){return d;})
+  var header = myTab.append('thead')
+    .selectAll('th')
+    .data(tableNames)
+    .enter()
+    .append('th')
+    .text(function(d){return d;})
 
-var tableBody = myTab.append('tbody')
-var row = tableBody.selectAll('tr')
-  .data(data)
-  .enter()
-  .append('tr')
-  .text(function(d,i){return d.Rank})
-  row.append('td')
-  .text(function(d,i){return d.Class})
-  row.append('td')
-  .text(function(d,i){return d.Name})
-  row.append('td')
-  .text(function(d,i){return d.Rating})
-  row.append('td')
-  .text(function(d,i){return d.InState})
-  row.append('td')
-  .text(function(d,i){return d.Total})
-
+  var tableBody = myTab.append('tbody')
+  var row = tableBody.selectAll('tr')
+    .data(data)
+    .enter()
+    .append('tr')
+    row.append('td')
+    .text(function(d,i){return d.Rank})
+    row.append('td')
+    .text(function(d,i){return d.Class})
+    row.append('td')
+    .text(function(d,i){return d.Name})
+    row.append('td')
+    .text(function(d,i){return d.Rating})
+    row.append('td')
+    .text(function(d,i){return d.InState})
+    row.append('td')
+    .text(function(d,i){return d.Total})
 }
