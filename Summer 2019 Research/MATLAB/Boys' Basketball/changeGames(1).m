275593cc008cb07b82f63d27976c9efacabdd565
Jackson Norris <jacksonnorris@Jacksons-MBP.attlocal.net>
2020-08-03 00:58:57 -0400

    j

--- Summer 2019 Research/MATLAB/Boys' Basketball/changeGames(1).m
+++ Summer 2019 Research/MATLAB/Boys' Basketball/changeGames(1).m
@@ -1,31 +1,16 @@
 function game = changeGames(game)
-%This function changes the PD of overtime wins to 0.5, removes forfeits,
-%and removes team who have played no games
 %% Change overtime
-overtime = find(game.OT == 1);
-game.PD(overtime) = 1/2;
-%% Remove forfeits
-forfeit = find(game.Forfeits == 1);
-game.Game(forfeit,:) = [];
-game.PD(forfeit) = []; 
-game.Loc(forfeit) = []; 
-game.Forfeits(forfeit) = [];
-game.Name(end);
-%% Remove teams that have no games
-%If there are no games, we want to remove from the game matrix, name matrix, and region matrix
-noGames = find(sum(abs(game.Game))==0) %Finds instances where sum of column is 0 
-game.Game(:,noGames) = [];
-
-%game.Name(noGames) = []; %comment back later 
-% Region(noGames) = [];
-game.OofS(:,noGames) = [];
-
-for elem = 1:length(game.Game(1,:))
-    if sum(game.Game(:,elem)) == 0
-        game.Name(elem);
+for elem = 1:length(game.OT) 
+    if game.OT(elem) == 1
+        game.PD(elem) = 1/2;
     end
 end
-find(sum(abs(game.Game))==0);
-
-
-%We don't remove from PD or Loc because there were no games played
+%% Remove forfeits
+for elem = length(game.Game):-1:1 
+    if game.Forfeits(elem) == 1 
+        game.Game(elem,:) = [];
+        game.PD(elem) = [];
+        game.Loc(elem) = [];
+        game.Forfeits(elem) = [];
+    end
+end
--- Summer 2019 Research/MATLAB/Boys' Basketball/masseyRating(1).m
+++ Summer 2019 Research/MATLAB/Boys' Basketball/masseyRating(1).m
@@ -1,58 +1,36 @@
-function [Massey,offense,defense] = masseyRating(Games,Teams)
-%% Massey's Equation
-% Mr = p 
-        %Mii represents the number of games played by team i 
-        %Mij (i~=j) represents the -number of games team i played against team j. 
-    %p is the point-differential column vector 
-    %r is the ratings     
-nTeams = length(Teams);
-M = zeros(nTeams,nTeams); %Initialize M with zeros
-pd = zeros(nTeams,1);  
-%p_for represents the points for and p_against represents points against 
-p_for = zeros(nTeams,1); 
-p_against = zeros(nTeams,1); 
-for game = 1:length(Games)
-    i = Games(game).winID; %i is the winner
-    j = Games(game).loseID; %j is the loser
-
-    %Find points of winner and loser
-    points_winner = Games(game).winScore; 
-    points_loser = Games(game).loseScore;
-    
-    point_differential = Games(game).PD;
-    if i==0 || j==0  %Remove out of state games
-        continue 
+function Massey = masseyRating(game)
+Game = game.Game; 
+PD = game.PD;
+Loc = game.Loc; 
+%% Initialize Variables
+hfa = 2.0; %Constant representing the home field advantage
+htw = 1; %If the home team wins by less than the home field advantage, the 
+         %winning team will still "win" by tis amount (ANALYZE/THINK ABOUT)
+pointGap = 35; %This is the max number that a team can win by 
+%% Iterate through game loop
+for elem = 1:length(PD) %This loop eliminates the huge point gap and codifies HFA
+    if PD(elem) >= pointGap
+        PD(elem) = pointGap;
     end
-    %%Change Diagonal Matrix%%
-    M(i,i) = M(i,i) + 1; %Add one to the diagonal entries
-    M(j,j) = M(j,j) + 1;
-    M(i,j) = M(i,j) - 1; %Subtract one off the diagonal
-    M(j,i) = M(j,i) - 1; 
-    %%Change Point Differential Matrix
-    pd(i) = pd(i) + point_differential;
-    pd(j) = pd(j) - point_differential;
-    
-    %Calculate points for and against
-    p_for(i) = p_for(i) + points_winner;
-    p_for(j) = p_for(j) + points_loser;
-    p_against(i) = p_against(i) + points_loser;
-    p_against(j) = p_against(j) + points_winner;
-    
-end
-%Split into diagonal and off-diagonal entries. (Used for offense and defense)
-T = diag(diag(M)); %% Amazingly, gets rid of off diagonal entries. 
-P = T - M;
+    if Loc(elem) == 1 %Home team wins
+        if PD(elem) < hfa
+            PD(elem) = PD(elem) - hfa;%htw;
+        elseif PD(elem) >= 3
+            PD(elem) = PD(elem) - hfa;
+        end
+    elseif Loc(elem) == 0 %Home team loses 
+        PD(elem) = PD(elem) + hfa;
+    elseif Loc(elem) == 2 %Neutral location (no hfa) 
+        PD(elem) = PD(elem);
+    end
+end 
 
-%M is not invertible. We must make some changes. 
-    %Add a row of ones to the nth element. Then add a zero to the nth
-    %element of the PD. 
-M(end,:) = ones(1, nTeams);
-pd(end) = 0; 
-r = inv(M)*pd;
-Massey = r; 
+%% Make final calculations
 
-%% Create point spread 
-%(T+P)*d = T*r - f;
-defense = inv(T+P)* (T*r - p_for); 
-offense = r - defense; %Offense(i) represents number of points team i is expected 
-                        %to score against average opponent
+len = size(Game,2); %Find number of teams
+Game = [Game; ones(1,len)]; %Add ones to the last row in the game matrix
+PD = [PD; 0]; %Add a zero to the last column in the game matrix
+M = Game'*Game; %Multiply the inverse of the game time the Game (document calls this M)
+
+%%FINAL CALCULATION%%
+Massey=inv(M)*Game'*PD;
