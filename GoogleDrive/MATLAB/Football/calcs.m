a018a23374bf8687424608c659b4e0b6d5de5ff7
jacksonkennedynorris <jackson.norris@centre.edu>
2020-09-02 16:03:26 -0400

    j

---
+++ GoogleDrive/MATLAB/Football/calcs.m
@@ -0,0 +1,5 @@
+function [Games,Teams] = calcs(Games,Teams,year)
+
+Massey = massey(Games,Teams);
+Colley = colley(Games,Teams,0);
+Elo = elo(Games,Teams);
--- GoogleDrive/MATLAB/Football/main.m
+++ GoogleDrive/MATLAB/Football/main.m
@@ -1,8 +1,9 @@
 function main
 clear all 
 close all 
-sport = "Football"; 
+sport = "Football";  
 for year = 2019 
     [Games,Teams,saveFinal] = loadingMatrices(year); 
-
+    [Games,Teams] = removeTeamsNoGames(Games,Teams);    
+    [Games,Teams] = calcs(Games,Teams,year);
 end
