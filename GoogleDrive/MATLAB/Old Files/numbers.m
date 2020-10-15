c2fe64013b382fcc20e5462b24d549714a5ea41a
jacksonkennedynorris <jackson.norris@centre.edu>
2020-10-14 22:45:28 -0400

    fixed the index hahahaha

---
+++ GoogleDrive/MATLAB/Old Files/numbers.m
@@ -0,0 +1,15 @@
+
+
+lecture_quizzes = 9*30/12
+engagement = 89
+homework = 97.41
+others = 76*50/80
+midterm = 79.05
+final = 82
+final = final*150/100
+points = lecture_quizzes + engagement + homework + others + midterm + final
+
+total_points = 30+100+100+50+100+150 
+
+
+points/total_points
