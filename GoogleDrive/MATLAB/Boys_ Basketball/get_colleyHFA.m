47e3d7b98f028da889e3f8e24aca58ddc8669648
Shelby Grace Montgomery <shelbymontgomery@Shelbys-MacBook-Pro.local>
2020-10-30 14:17:32 -0400

    addition of massey function

---
+++ GoogleDrive/MATLAB/Boys_ Basketball/get_colleyHFA.m
@@ -0,0 +1,73 @@
+function [percentCorrectColley] = get_colleyHFA(year,sport,numofDays)
+%Test the ratings
+%% Accumulators
+
+numofDays = 92; %rather than the day
+sport = "Boy's Basketball";
+
+%Values added to a win or loss later in this code
+HFA_values = 0.002:0.001:0.02; %0.02:0.002:0.05;
+%Used inside the colley function
+HFA_InsideRatingFunction = 0.03:0.01:0.1; %0.03:0.01:0.1;
+
+%Create empty vectors for loop
+Coll_correct_pred=zeros(length(HFA_values),length(HFA_InsideRatingFunction));
+Colltotal=zeros(length(HFA_values),length(HFA_InsideRatingFunction));
+
+%% Loop through each year to determine if ratings are inline with predictions
+ for day=35:numofDays-1
+%% Load matrices for old days and days+1
+    [Games,Teams,~] = loadingMatrices(year,day);
+    Games,Teams
+    [Games,Teams]= modifyData(Games,Teams);
+    Games,Teams
+    [NewGames,NewTeams,~] = loadingMatrices(year,day+1,day+1);
+    NewGames,NewTeams
+    i=0;
+        for HFA = HFA_values
+            i=i+1;
+            j=0;
+            for HFA_Inside = HFA_InsideRatingFunction
+                j=j+1;
+                colleyRatings = colleyRating(Games,Teams,HFA_Inside);
+
+        %% Determine result of game
+                 for elem = 1:size(NewGames,1)
+
+                    if Games(elem).winID==0||Games(elem).loseID==0
+                        continue
+                    end
+
+                     NewLoc = NewGames(elem).Loc;
+
+                    %Actual result of game
+                    actual_winner=NewGames(elem).winID;
+                    actual_loser=NewGames(elem).loseID
+
+                    % Get Colley ratings
+                    collWin=colleyRatings(actual_winner)
+                    collLos=colleyRatings(actual_loser)
+                    %% HFA and add to accumulators
+                    if NewLoc==1
+                        collWin=collWin+HFA;
+                    elseif NewLoc==0
+                        collWin=collWin-HFA;
+                    end
+
+                    if collWin>collLos
+                        Coll_correct_pred(i,j)=Coll_correct_pred(i,j)+1;
+                        Colltotal(i,j)=Colltotal(i,j)+1;
+                    else
+                        Colltotal(i,j)=Colltotal(i,j)+1;
+                    end
+
+                end
+            end
+        end
+ end
+
+
+
+%% Calculate percentages!
+
+percentCorrectColley= (Coll_correct_pred./Colltotal).*100
---
+++ GoogleDrive/MATLAB/Boys_ Basketball/masseyRatingW.m
@@ -0,0 +1,62 @@
+function Massey = masseyRatingW(Games,Teams,hfa)
+%%%%% masseyRatingW includes WEIGHTS
+%% Initialize Variables
+[newData,Games,Teams] = createGameMat(Games,Teams);
+Game= newData.Game;
+PD= newData.PD;
+Loc= newdata.Loc;
+htw = 1; %If the home team wins by less than the home field advantage, the
+         %winning team will still "win" by tis amount (ANALYZE/THINK ABOUT)
+pointGap = 35; %This is the max number that a team can win by
+%% Iterate through game loop
+%Took out all the elems
+for elem = 1:length(PD) %This loop eliminates the huge point gap and codifies HFA
+
+    if PD(elem) >= pointGap
+        PD(elem) = pointGap;
+    end
+    %elem
+    %length(Loc)
+    %Loc(elem)
+    if Loc(elem) == 1 %Home team wins
+        if PD(elem) < hfa
+            PD = PD(elem) - hfa;%htw;
+        elseif PD(elem) >= 3
+            PD = PD(elem) - hfa;
+        end
+    elseif Loc(elem) == 0 %Home team loses
+        PD = PD(elem) + hfa;
+    elseif Loc(elem) == 2 %Neutral location (no hfa)
+        PD = PD(elem);
+    end
+end
+
+%% Make final calculations
+l = length(Game(:,1));
+%length(PD)
+%length(Loc)
+
+len = size(Game,2); %Find number of teams
+Game = [Game; ones(1,len)]; %Add ones to the last row in the game matrix
+PD = [PD; 0]; %Add a zero to the last column in the game matrix
+M = Game'*Game; %Multiply the inverse of the game time the Game (document calls this M)
+Massey = inv(M)*Game'*PD;
+Game(end,:)=[]; PD(end)=[];
+oldMassey=zeros(length(Massey),1);
+
+while norm(Massey-oldMassey) > 0.0001
+
+    predMargin = get_massey_prediction(Game,Massey,Loc);
+
+    weights = calc_massey_weights(predMargin,PD);
+
+    weightedGame=weights.*Game;
+    weightedPD=weights.*PD;
+    weightedGame = [weightedGame; ones(1,len)]; %Add ones to the last row in the game matrix
+    weightedPD = [weightedPD; 0];
+    M_adj=weightedGame'*weightedGame;
+
+    oldMassey=Massey;
+    Massey=inv(M_adj)*weightedGame'*weightedPD;
+
+end
