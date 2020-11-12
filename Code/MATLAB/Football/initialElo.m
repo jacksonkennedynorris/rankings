7a58e8564936e2f18c154e3e9e2df4a4726afdc2
Jackson Norris <jacksonnorris@Jacksons-MacBook-Pro.local>
2020-11-12 10:44:30 -0500

    Establish Index vector

--- Code/MATLAB/Football/initialElo.m
+++ Code/MATLAB/Football/initialElo.m
@@ -6,22 +6,20 @@ else
     cd Teams 
         lastName = readtable(strcat(num2str(year-1),'_teams.txt'));
         thisName = readtable(strcat(num2str(year),'_teams.txt'));
-        [~, index] = ismember(lastName.Name, thisName.Name);
+        [~, index] = ismember(thisName.Name,lastName.Name);
         index
-        %merged = innerjoin(lastName,thisName, 
     cd ..
-    if ismac 
-       for i = 1:length(index)
-            if index[i] == 0
-                i
-            end
+    for i = 1:length(index)
+        if index(i) == 0 %% Initialize at 1500 
+            i
         end
-      Elo = load(strcat('Ratings/',num2str(year-1),'/elorating.txt'));
-       initElo = (1/3)*(1500 + 2*Elo);       
+    end
+    if ismac 
+      Elo = load(strcat('Ratings/',num2str(year-1),'/elorating.txt'));       
     end
     if ispc
         Elo = load(strcat('Ratings\',num2str(year-1),'\elorating.txt'));
-        initElo = (1/3)*(1500 + 2*Elo);
     end
+    initElo = (1/3)*(1500 + 2*Elo);
 end
 
