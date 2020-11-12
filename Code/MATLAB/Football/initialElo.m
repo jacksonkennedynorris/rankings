0672b5a926568d19b6981dae8c4465b4c7d0e27e
jacksonkennedynorris <jackson.norris@centre.edu>
2020-11-12 09:58:49 -0500

    working through the elo mismatches

--- Code/MATLAB/Football/initialElo.m
+++ Code/MATLAB/Football/initialElo.m
@@ -3,13 +3,25 @@ function initElo = initialElo(Games, Teams, year)
 if year == 1998 
     initElo = 1500*ones(1,length(Teams));
 else
+    cd Teams 
+        lastName = readtable(strcat(num2str(year-1),'_teams.txt'));
+        thisName = readtable(strcat(num2str(year),'_teams.txt'));
+        [~, index] = ismember(lastName.Name, thisName.Name);
+        index
+        %merged = innerjoin(lastName,thisName, 
+    cd ..
     if ismac 
-        Elo = load(strcat('Ratings/',num2str(year-1),'/elorating.txt'));
-        initElo = (1/3)*(1500 + 2*Elo);
-    elseif ispc
+       for i = 1:length(index)
+            if index[i] == 0
+                i
+            end
+        end
+      Elo = load(strcat('Ratings/',num2str(year-1),'/elorating.txt'));
+       initElo = (1/3)*(1500 + 2*Elo);       
+    end
+    if ispc
         Elo = load(strcat('Ratings\',num2str(year-1),'\elorating.txt'));
         initElo = (1/3)*(1500 + 2*Elo);
     end
 end
 
-initElo = 1500*ones(1,length(Teams));
