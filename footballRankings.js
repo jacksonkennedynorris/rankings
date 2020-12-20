e598bf54318a444ae066e8cb5e588a2caeaedd6c
Jackson Norris <jackson.norris@centre.edu>
2020-12-20 13:30:22 -0500

    Final 2020 rankings!

--- footballRankings.js
+++ footballRankings.js
@@ -1,15 +1,18 @@
-var tableData = d3.csv('football/teams_2020.csv')
-  tableData.then(function(data){useData(data)},
+var link = "football/teams_2020.csv"
+var year = link.substring(15,19)
+
+var tableData = d3.csv(link)
+  tableData.then(function(data){useData(data,year)},
   function(err){console.log(err)});
 
-var useData = function(data){
+var useData = function(data,year){
     width = 500;
     height = 1000;
 
-    var svg = d3.select('svg')
+    /*var svg = d3.select('svg')
     .attr('width',width)
     .attr('height',height)
-
+*/
   var myTab = d3.select('table.totalTable')
 
   var tableNames = ["Ranking","Name",/*"Region",*/"Rating",/*"Record"*/"Wins","Losses","Massey","Colley","Elo"];
@@ -29,6 +32,7 @@ var useData = function(data){
     .append('tr')
     row.append('td')
     .text(function(d,i){return i+1})
+    .attr('class', 'ranking')
     row.append('td')
     .text(function(d,i){
       /*return d.name })*/
@@ -48,23 +52,29 @@ var useData = function(data){
       else {
         return d.name + " (" + d.region + "A)"}
       })
+    .attr('class','name')
     /*row.append('td')
     .text(function(d,i){return d.region})*/
     row.append('td')
     .text(function(d,i){return d.rating})
+    .attr('class','rating')
     /*
     row.append('td')
     .text(function(d,i){return d.wins + " - " + d.losses})*/
-
     row.append('td')
     .text(function(d,i){return d.wins})
+    .attr('class','wins')
     row.append('td')
     .text(function(d,i){return d.losses})
+    .attr('class','losses')
     row.append('td')
     .text(function(d,i){return d.massey})
+    .attr('class','massey')
     row.append('td')
     .text(function(d,i){return d.colley})
+    .attr('class','colley')
     row.append('td')
     .text(function(d,i){return d.elo})
+    .attr('class','elo')
     console.log(data)
 }
